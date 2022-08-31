8fb1e103cfeb6251a8f6cec10b65e91c6fdeff94
Christ0pheri <github@acc.aeh-dings-hier.de>
2022-08-31 23:08:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-import requests, re, time
+import requests, re, time, phonenumbers
 
 import settings
 
-def FindPads(baseurl = "", padnames = [], urlextention = "", regex = "", verbose = False, sleep = False):
+def FindPads(baseurl = "", padnames = [], phone_numbers = [], urlextention = "", regex = "", verbose = False, sleep = False):
 	for name in padnames:
 		if verbose:
 			print("Pad", name, "durchsuchen")
 		text = OpenPads(baseurl = baseurl, padname = name, urlextention = urlextention)
 		ParseText(text, regex = regex, baseurl = baseurl, padnames = padnames, verbose = verbose)
-		ParsePhoneNumbers(text, regex = "\+?\d{2,5}\s?(?:\(\d\))?\s?\d{3,4}\s?\d+", phonenumbers = [], verbose = verbose)
+		ParsePhoneNumbers(text, regex = "\+?\d{2,5}\s?(?:\(\d\))?\s?\d{3,4}\s?\d+", phone_numbers = phone_numbers, padname = name, verbose = verbose)
 		if sleep:
-			time.sleep(5)
+			time.sleep(2.5)
 
 def OpenPads(baseurl = "", padname = "", urlextention = ""):
 	url = baseurl + padname + urlextention
@@ -31,13 +31,15 @@ def ParseText(text, regex = "", baseurl= "", padnames = [], verbose = False):
 			#print(index, padname)
 			padnames.append(padname)
 
-def ParsePhoneNumbers(text, regex = "", phonenumbers = [], verbose = False):
-	numbers = re.findall(regex, text)
-	for number in numbers:
+def ParsePhoneNumbers(text, regex = "", phone_numbers = [], padname = "", verbose = False):
+	matches = phonenumbers.PhoneNumberMatcher(text, "IN")
+	for match in matches:
+		number = phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164)
+		if number not in phone_numbers:
+			phone_numbers.append((number, padname))
 		if verbose:
-			print(number)
-			time.sleep(10)
-
+			print(number, "in", padname, "gefunden")
+			time.sleep(2.5)
 if __name__ == "__main__":
 	import sys
 
@@ -51,10 +53,13 @@ if __name__ == "__main__":
 			elif arg == "-s":
 				sleep = True
 	padnames = settings.padnames
+	phone_numbers = []
 
-	FindPads(baseurl = settings.baseurl, padnames = padnames, urlextention = settings.urlextention, regex = settings.regex, verbose = verbose, sleep = sleep)
+	FindPads(baseurl = settings.baseurl, padnames = padnames, phone_numbers = phone_numbers, urlextention = settings.urlextention, regex = settings.regex, verbose = verbose, sleep = sleep)
 
 	if verbose:
 		print(len(padnames), " Pads gefunden")
 		padnames.sort()
 		print(padnames)
+		phone_numbers.sort()
+		print(phone_numbers)
