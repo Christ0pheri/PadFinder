07632823b93ec875213fc3827207f4fa5ec59c8d
Christ0pheri <github@acc.aeh-dings-hier.de>
2022-08-31 03:16:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def FindPads(baseurl = "", padnames = [], urlextention = "", regex = "", verbose
 			print("Pad", name, "durchsuchen")
 		text = OpenPads(baseurl = baseurl, padname = name, urlextention = urlextention)
 		ParseText(text, regex = regex, baseurl = baseurl, padnames = padnames, verbose = verbose)
-
+		ParsePhoneNumbers(text, regex = "^(?:\d{6,}|\d{3,5}\s\d{3,}|\+\d{2,4}\s\d{2,}\s\d{3,}|\+\d{2,4}\s\(0\)\d{2,}\s\d{3,})$", phonenumbers = [], verbose = verbose)
 		if sleep:
 			time.sleep(5)
 
@@ -30,6 +30,12 @@ def ParseText(text, regex = "", baseurl= "", padnames = [], verbose = False):
 				print(padname, "zu Pads hinzufügen")
 			#print(index, padname)
 			padnames.append(padname)
+
+def ParsePhoneNumbers(text, regex = "", phonenumbers = [], verbose = False):
+	numbers = re.findall(regex, text)
+	for number in numbers:
+		if verbose:
+			print(number)
 
 if __name__ == "__main__":
 	import sys
