e2e1430c61f71282737cd85d957b2e89640313ca
Christ0pheri <github@acc.aeh-dings-hier.de>
2022-09-01 15:07:52 +0200

    New handling for too many requests
    pad.fridaysforfuture.is returning "Too many requests, please try again later." without raising an exeption.

--- app.py
+++ app.py
@@ -2,21 +2,24 @@ import requests, re, time, urllib.parse, phonenumbers
 
 import settings
 
-def FindPads(filepath = "", baseurl = "", padnames = [], phone_numbers = [], urlextention = "", regex = "", verbose = False, sleep = False):
-	ParseChats(filepath, baseurl = baseurl, padnames = padnames, urlextention = urlextention, regex = regex, verbose = verbose, sleep = sleep)
+def FindPads(filepath = "", baseurl = "", padnames = [], phone_numbers = [], urlextention = "", regex = "", verbose = False):
+	ParseChats(filepath, baseurl = baseurl, padnames = padnames, urlextention = urlextention, regex = regex, verbose = verbose)
 	for name in padnames:
 		if verbose:
 			print("Pad", name, "durchsuchen")
-		text = OpenPads(baseurl = baseurl, padname = name, urlextention = urlextention)
+		text = OpenPads(baseurl = baseurl, padname = name, urlextention = urlextention, verbose = verbose)
 		ParseText(text, regex = regex, baseurl = baseurl, padnames = padnames, verbose = verbose)
 		ParsePhoneNumbers(text, regex = "\+?\d{2,5}\s?(?:\(\d\))?\s?\d{3,4}\s?\d+", phone_numbers = phone_numbers, padname = name, verbose = verbose)
-		if sleep:
-			time.sleep(2.5)
 
-def OpenPads(baseurl = "", padname = "", urlextention = ""):
+def OpenPads(baseurl = "", padname = "", urlextention = "", verbose = False, sleep = 30):
 	url = baseurl + padname + urlextention
 	result = requests.get(url)
 	text = result.text
+	if text == "Too many requests, please try again later.": #result doesn't raise an exeption
+		if verbose:
+			print("Too many requests, trying again in {0} seconds.".format(sleep))
+		time.sleep(sleep)
+		return OpenPads(baseurl = baseurl, padname = padname, urlextention = urlextention, verbose = verbose)
 	return text
 
 def ParseText(text, regex = "", baseurl= "", padnames = [], verbose = False):
@@ -39,30 +42,46 @@ def ParsePhoneNumbers(text, regex = "", phone_numbers = [], padname = "", verbos
 			phone_numbers.append((number, padname))
 		if verbose:
 			print(number, "in", padname, "gefunden")
-			time.sleep(2.5)
+			#time.sleep(5)
 
-def ParseChats(filepath, baseurl = "", padnames = [], urlextention = "", regex = "", verbose = False, sleep = False):
+def ParseChats(filepath, baseurl = "", padnames = [], urlextention = "", regex = "", verbose = False):
 	with open(filepath) as f:
 		content = f.read()
 	ParseText(content, regex = regex, baseurl= baseurl, padnames = padnames, verbose = False)
+
+def ExpPads(filepath, baseurl = "", padnames = [], verbose = False):
+	padnames.sort()
+	with open(filepath, "w") as f:
+		for padname in padnames:
+			url = baseurl + padname
+			f.write(url + "\n")
+def ExpNumbers(filepath, baseurl = "", phone_numbers = [], verbose = False):
+	phone_numbers.sort()
+	with open(filepath, "w") as f:
+		for phonenumber in phone_numbers:
+			number = phonenumber[0]
+			padname = phonenumber[1]
+			url = baseurl + padname
+			f.write("{0} in {1} gefunden\n".format(number, url))
 
 if __name__ == "__main__":
 	import sys
 
 	verbose = False
-	sleep = True
 
 	if len(sys.argv) > 1:
 		for index, arg in enumerate(sys.argv[1:], 1):
 			if arg == "-v":
 				verbose = True
-			elif arg == "-s":
-				sleep = False
 
 	padnames = settings.padnames
 	phone_numbers = []
 
-	FindPads(filepath = settings.filepath, baseurl = settings.baseurl, padnames = padnames, phone_numbers = phone_numbers, urlextention = settings.urlextention, regex = settings.regex, verbose = verbose, sleep = sleep)
+	FindPads(filepath = settings.filepath, baseurl = settings.baseurl, padnames = padnames, phone_numbers = phone_numbers, urlextention = settings.urlextention, regex = settings.regex, verbose = verbose)
+	#filepath = settings.filepath
+	ExpPads(settings.filepath_export + "pads.txt", baseurl = settings.baseurl, padnames = padnames, verbose = verbose)
+
+	ExpNumbers(settings.filepath_export + "numbers.txt", baseurl = settings.baseurl, phone_numbers = phone_numbers, verbose = verbose)
 
 	if verbose:
 		print(len(padnames), " Pads gefunden")
